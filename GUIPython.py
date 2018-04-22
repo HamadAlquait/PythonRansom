8f7dcd7f0317d25b6bead9341cd15e1800f47b0c
MYCOMPUTER\Lebron James <kingcelticsjay@gmail.com>
2018-04-21 23:06:33 -0500

    Updated

--- GUIPython.py
+++ GUIPython.py
@@ -1,33 +1,92 @@
+import tkMessageBox
+import ttk
 from Tkinter import *
+from PIL import Image, ImageTk
+import time
+import sys
+
 
 # Main part of the program that has the background and window sizes
 root = Tk()
 root.geometry("900x600")
 root.title("This is a RansomWare Attack")
 root.configure(background="black")
+root.overrideredirect(True)
+tempkey = "JacobIsAGreatCoder"
+
+def submit_function():
+    count = 0
+    while count < 1:
+            tkMessageBox.showinfo("CHECKING KEY...", "FILES NOW DECRYPTED. THANK YOU!!!")
+            count += 1
+    exit()
+
+def Messages():
+    global entering
+    string = entering.get()
+    if string == tempkey:
+
+        print string
+        tkMessageBox.showinfo("Checker", "YOUR FILES ARE NOW SAFE. THANK YOU!!!")
+        exit()
+    else:
+        tkMessageBox.showerror("ERROR", "Your Key Entered Is Not Correct!! REMOVING TIME!!")
 
 
+def submit_denied():
+    tkMessageBox.showinfo("ACCESS DENIED", "KEY NOT VALID. TRY AGAIN!!!!")
+
+def countdown_timer(self):
+    counter = 25
+ #   while counter > 0:
+
+def ListRandomNumbers():
+    scrolling = Scrollbar(root).place(x=100, y=70, height=350)
+    ListBoxText = Listbox(root, height=2, width=15, bg="RED")
+    ListBoxText.place(x=1, y=70)
+    ListBoxText.insert(0, "000110101101010")
+    ListBoxText.insert(0, "001010101001010")
+    ListBoxText.insert(0, "110100101010010")
+    #ListBoxText.insert(0, "42  70")
+    #ListBoxText.place(x=3, y=70)
+    #for random_numbers in [ListBoxText]:
+        #ListBoxText.insert(END, random_numbers)
 
 
 #This is the main banner at the top of the page
 FirstHeading = Label(root, relief="raised", text="THIS IS A RANSOMWARE ATTACK ", font=("helvetica", 40, "bold"), bg="PURPLE",
                      fg="RED").pack()
 #This is the text and submit box for the program
-entering = Entry(root, width=80, relief="sunken").place(x=250, y=500, height=28)
+entering = Entry(root, width=80, relief="sunken")
+entering.place(x=250, y=500, height=28)
+entering.focus_set()
 
 #SUBMIT button that is listening to a users actions
-button1 = Button(root, text="SUBMIT", fg="BLUE", relief="raised", bg="RED").place(x=750, y=500)
+button1 = Button(root, text="SUBMIT", fg="BLUE", relief="raised", command=Messages,
+            bg="RED").place(x=750, y=500)
 
+#COUNTDOWNTIMER
+timer = Label(root, font=("ARIAL", 40, "bold"), bg="RED", width=14).place(x=250, y=448, height=45)
 
-SideLabel = Label(root, text="HAHA").place(x=3, y=70, height=200)
-MainLabel = Label(root, text="WHAT HAPPENED TO YOUR FILES", relief="sunken", width=90).place(x=250, y=80, height=320)
+#This is where the layers are for the designed image
+ListRandomNumbers()
 
-Timer_Countdown = Label(root, text="PLACEHOLDER TIMER", font=("times", 30, "bold"), fg="GREEN", bg="WHITE", width=20).place(x=200, y=420)
-Chooser = Checkbutton(root, bg="BLUE", text="DECRYPT YOUR FILES").place(x=25, y=500);
-Chooser2 = Checkbutton(root, bg="BLUE", text="AVOID BRICKING SYSTEM").place(x=25, y=520);
+#CODE to display the description
+DescriptionImage = Image.open("Capture.GIF")
+tkimage = ImageTk.PhotoImage(DescriptionImage)
+LabelIt = Label(root, image=tkimage, relief="sunken").pack()
+
+#Bitcoin Image
+BitcoinImage = Image.open("bitcoin.png")
+BC = ImageTk.PhotoImage(BitcoinImage)
+LabelIt2 = Label(root, image=BC, height=100)
+LabelIt2.place(x=200, y=535)
+
+#These are to display the check button boxes
+Chooser = Checkbutton(root, bg="BLUE", text="DECRYPT YOUR FILES").place(x=25, y=500)
+Chooser2 = Checkbutton(root, bg="BLUE", text="AVOID BRICKING SYSTEM").place(x=25, y=520)
+Chooser3 = Checkbutton(root, bg="BLUE", text="PAY ME").place(x=25, y=540)
 #update_time()
-
-
 
 
 root.mainloop()
